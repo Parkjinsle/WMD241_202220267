d0f627e6fe15bd2eaede7bf722e28a5ad5e99767
Parkjinsle (Parkjinsle) <162863747+Parkjinsle@users.noreply.github.com>
2024-05-23 11:39:28 +0900

    중간

--- src/w12/stp2/script/script.js
+++ src/w12/stp2/script/script.js
@@ -1,7 +1,7 @@
 console.log('안녕하세요');
 let elemBody = document.body;
 console.log(elemBody);
-elemBody.style.backgroundColor = 'oklch(0.75, 0.05, 0)';
+elemBody.style.backgroundColor = 'oklch(0.8, 0.05, 0)';
 // style 넣으려면 '' 필수
 
 let aCard = document.querySelector('.card');
@@ -9,7 +9,11 @@ let aCard = document.querySelector('.card');
 console.log(aCard);
 aCard.addEventListener('click', function () {
   console.log('클릭!');
+  aCard.classList.toggle('card--state-clicked');
+  // aCard.style.backgroundColor = '#ff0000';
+  console.log(aCard.classList);
 });
+// html엘레먼트.addEventListener('이벤트종류', function () {할일});
 
 // document.querySelectorAll('.card');
 // .card에 해당하는 모든 친구들을 찾아줌
