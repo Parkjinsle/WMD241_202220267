add22aa14f34ddf5474ca7c2c2c4ca16a77a7b0d
Parkjinsle (Parkjinsle) <162863747+Parkjinsle@users.noreply.github.com>
2024-05-23 13:03:51 +0900

    12주차 끝

--- src/w12/stp4/script/script.js
+++ src/w12/stp4/script/script.js
@@ -1,6 +1,6 @@
 let elemBody = document.body;
 elemBody.style.backgroundColor = '#00ff00';
-let container = document.querySelector('.conrainer');
+let container = document.querySelector('.container');
 let cards = document.querySelectorAll('.card');
 for (let i = 0; i < cards.length; i++) {
   let eachCard = cards[i];
